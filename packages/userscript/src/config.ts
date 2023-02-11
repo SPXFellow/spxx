51305b50a5ac8c856d71c39fe63564b89e2bd91b
Ricolove <kakagou.mc@outlook.com>
2023-02-11 13:14:51 +0800

    v2.4.14 Providing bug center selection

--- packages/userscript/src/config.ts
+++ packages/userscript/src/config.ts
@@ -7,18 +7,24 @@ GM_config.init({
       type: 'text',
       default: 'SPXX User',
     },
+    bugSource: {
+      label: 'Translation Source',
+      type: 'select',
+      options: ['Guangyao', 'Github', 'Custom'],
+      default: 'Guangyao',
+    },
     bugCenterTranslation: {
-      label: 'Bugs Center translation source',
+      label: 'Custom translation source',
       type: 'text',
       default: 'https://bugs.guangyaostore.com/translations',
     },
     bugCenterTranslator: {
-      label: 'Bugs Center translator source',
+      label: 'Custom translator source',
       type: 'text',
       default: 'https://bugs.guangyaostore.com/translator',
     },
     bugCenterColor: {
-      label: 'Bugs Center color source',
+      label: 'Custom color source',
       type: 'text',
       default: 'https://bugs.guangyaostore.com/color',
     },
@@ -27,12 +33,35 @@ GM_config.init({
 
 GM_registerMenuCommand('Edit Configuration', () => GM_config.open())
 
+const src = GM_config.get('bugSource') as string
+let tr = ""
+let tor = ""
+let c = ""
+if (src == "Guangyao"){
+  console.log("[SPXX] Using Guangyao bug center")
+  tr = 'https://bugs.guangyaostore.com/translations'
+  tor = 'https://bugs.guangyaostore.com/translator'
+  c = 'https://bugs.guangyaostore.com/color'
+}
+else if (src == "Github"){
+  console.log("[SPXX] Using Github bug center")
+  tr = 'https://raw.githubusercontent.com/SPXFellow/spxx-translation-database/crowdin/zh-CN/zh_CN.json'
+  tor = 'https://raw.githubusercontent.com/SPXFellow/spxx-translation-database/master/translator.json'
+  c = 'https://raw.githubusercontent.com/SPXFellow/spxx-translation-database/master/color.json'
+}
+else{
+  console.log("[SPXX] Using custom bug center")
+  tr = GM_config.get('bugCenterTranslation') as string
+  tor = GM_config.get('bugCenterTranslator') as string
+  c = GM_config.get('bugCenterColor') as string
+}
+
 const config = {
   translator: GM_config.get('translator') as string,
   bugCenter: {
-    translation: GM_config.get('bugCenterTranslation') as string,
-    translator: GM_config.get('bugCenterTranslator') as string,
-    color: GM_config.get('bugCenterColor') as string,
+    translation: tr,
+    translator: tor,
+    color: c,
   },
 }
 
